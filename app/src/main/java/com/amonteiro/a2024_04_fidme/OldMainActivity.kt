9eec236dabf766881631414d4988fb0925c3f6ee
Anthony <anth06ny@gmail.com>
2024-05-14 12:39:38 +0200

    TP IHM

---
+++ app/src/main/java/com/amonteiro/a2024_04_fidme/OldMainActivity.kt
@@ -0,0 +1,17 @@
+package com.amonteiro.a2024_04_fidme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OldMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_old_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
